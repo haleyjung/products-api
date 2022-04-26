ecb67c0cc6ea549435509577f4b0af315f01afa9
haleyjung <haleyjung16@gmail.com>
2022-04-26 02:45:03 -0400

    update unit tests

--- server/__tests__/api/api_spec.js
+++ server/__tests__/api/api_spec.js
@@ -1,47 +1,54 @@
 const frisby = require('frisby');
 
-let product_id = 37311;
-let baseURL = `http://localhost:3030/products/${product_id}/`;
+let baseURL = `http://localhost:3030/products`;
 
+/**
+ * Testing get a single product endpoint
+ */
 describe('Product', function () {
   it ('GET request should return a status of 200 OK', function () {
   return frisby
-    .get(`${baseURL}`)
+    .get(`${baseURL}/37311`)
     .expect('status', 200);
   });
 
   it ('should have a JSON Content-Type header', function () {
     return frisby
-      .get(`${baseURL}`)
+      .get(`${baseURL}/37311`)
       .expect('header', 'Content-Type', 'application/json; charset=utf-8');
   });
 });
 
+/**
+ * Testing get styles endpoint
+ */
 describe('Styles', function () {
   it ('GET request should return a status of 200 OK', function () {
     return frisby
-      .get(`${baseURL}`)
+      .get(`${baseURL}/37311/styles`)
       .expect('status', 200);
   });
 
   it ('should have a JSON Content-Type header', function () {
     return frisby
-      .get(`${baseURL}related`)
-      .expect('status', 200);
-  });
-});
-
-describe('Related Products', function () {
-  it ('GET request should return a status of 200 OK', function () {
-    return frisby
-      .get(`${baseURL}styles`)
-      .expect('status', 200);
-  });
-
-  it ('should have a JSON Content-Type header', function () {
-    return frisby
-      .get(`${baseURL}styles`)
+      .get(`${baseURL}/37311/styles`)
       .expect('header', 'Content-Type', 'application/json; charset=utf-8');
   });
 });
 
+/**
+ * Testing get related products endpoint
+ */
+describe('Related Products', function () {
+  it ('GET request should return a status of 200 OK', function () {
+    return frisby
+      .get(`${baseURL}/37311/related`)
+      .expect('status', 200);
+  });
+
+  it ('should have a JSON Content-Type header', function () {
+    return frisby
+      .get(`${baseURL}/37311/related`)
+      .expect('header', 'Content-Type', 'application/json; charset=utf-8');
+  });
+});
