b3b82f38a5cf4a3276faad12943113d815116e2e
haleyjung <haleyjung16@gmail.com>
2022-04-26 00:59:52 -0400

    update pg config

--- server/db.js
+++ server/db.js
@@ -1,121 +1,17 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-const { PG_USER, PG_PASSWORD, PG_PORT } = process.env;
+const { PG_USER, PG_PASSWORD, PG_HOST, PG_PORT, PG_DBNAME } = process.env;
 
 const pool = new Pool({
   user: PG_USER,
   password: PG_PASSWORD,
-  host: "localhost",
+  host: PG_HOST,
   port: PG_PORT,
-  database: "products"
+  database: PG_DBNAME
 });
 
 pool.connect()
   .then(() => console.log('connected!'))
   .catch((err) => console.log('connection error:', err.message));
 
-  // const poolWrapper = {
-  //     async connect() {
-  //         for (let nRetry = 1; ; nRetry++) {
-  //             try {
-  //                 const client = await pool.connect();
-  //                 if (nRetry > 1) {
-  //                     console.info('Now successfully connected to Postgres');
-  //                 }
-  //                 return client;
-  //             } catch (e) {
-  //                 if (e.toString().includes('ECONNREFUSED') && nRetry < 5) {
-  //                     console.info('ECONNREFUSED connecting to Postgres, ' +
-  //                         'maybe container is not ready yet, will retry ' + nRetry);
-  //                     // Wait 1 second
-  //                     await new Promise(resolve => setTimeout(resolve, 1000));
-  //                 } else {
-  //                     throw e;
-  //                 }
-  //             }
-  //         }
-  //     }
-  // };
-
 module.exports = pool;
-
-// const { Sequelize, DataTypes } = require('sequelize');
-// require('dotenv').config();
-// const { PG_USER, PG_PASSWORD, PG_PORT, PG_DBNAME } = process.env;
-
-// const user = PG_USER;
-// const password = PG_PASSWORD;
-// const host = "localhost";
-// const port = PG_PORT;
-// const database = PG_DBNAME;
-
-// const db = new Sequelize(database, user, password, {
-//   host: host,
-//   port: port,
-//   dialect: 'postgres',
-//   logging: false,
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// });
-
-// const Product = db.define('product', {
-//   id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true,
-//   },
-//   name: DataTypes.STRING,
-//   slogan: DataTypes.STRING(500),
-//   description: DataTypes.STRING(500),
-//   category: DataTypes.STRING,
-//   default_price: DataTypes.STRING,
-// }, {
-//   underscored: true,
-// });
-
-// const Styles = db.define('styles', {
-//   id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true,
-//   },
-//   name: DataTypes.STRING,
-//   original_price: DataTypes.STRING,
-//   sale_price: DataTypes.STRING,
-//   default_style: BOOLEAN,
-// }, {
-//   underscored: true,
-//   indexes: [{ fields: ['product_id']}],
-// });
-
-// const Features = db.define('features', {
-//   id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true,
-//   },
-//   feature: DataTypes.STRING(50),
-//   value: DataTypes.STRING(50)
-// }, {
-//   underscored: true,
-//   indexes: [{ fields: ['product_id']}],
-// })
-
-// const Photos = db.define('photos', {
-//   id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true,
-//   },
-//   thumbnail_url: DataTypes.TEXT,
-//   url: DataTypes.TEXT,
-// }, {
-//   underscored: true,
-//   indexes: [{ fields: ['styles_id'] }],
-// })
-
-// module.exports = { Product };
--- server/index.js
+++ server/index.js
@@ -2,22 +2,20 @@ const express = require('express');
 const app = express();
 const pool = require('./db');
 
-const cors = require("cors");
-const morgan = require("morgan");
+const cors = require('cors');
+const morgan = require('morgan');
 
 require('dotenv').config();
 const PORT = process.env.PORT;
 
-// Middlewares for logging and parsing
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Routes
 app.get('/', (req, res) => {
   res.send('SDC: products API!');
-})
+});
 
 app.get('/products/:product_id', (req, res) => {
   let product_id = req.params.product_id - 37310;
@@ -49,8 +47,10 @@ app.get('/products/:product_id', (req, res) => {
 
   return pool
     .query(queryString)
-    .then(result => { res.send(result.rows) })
-    .catch(err => console.error(err.message));
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => console.error(err.message));
 });
 
 app.get('/products/:product_id/styles', (req, res) => {
@@ -93,32 +93,28 @@ app.get('/products/:product_id/styles', (req, res) => {
 
   return pool
     .query(queryString)
-    .then(result => {
-
+    .then((result) => {
       let styles = result.rows[0].results;
-
-      // update styles id
       for (let i = 0; i < styles.length; i++) {
-        styles[i].style_id += 220997; // -> WORKS!
-
-        // update skus id (+ 1281031)
-        let skus = styles[i].skus; // -> obj with keys that need an update
-
+        styles[i].style_id += 220997;
+        let skus = styles[i].skus;
         for (let sku in skus) {
           let newKey = (Number(sku) + 1281031).toString();
           skus[newKey] = skus[sku];
           delete skus[sku];
         }
       }
-      res.send(result.rows)
+      res.send(result.rows);
     })
-    .catch(err => console.log(err.message));
+    .catch((err) => console.log(err.message));
 });
 
 app.get('/products/:product_id/related', (req, res) => {
   let product_id = req.params.product_id - 37310;
   pool
-    .query(`SELECT related_product_id from related WHERE current_product_id = ${product_id}`)
+    .query(
+      `SELECT related_product_id from related WHERE current_product_id = ${product_id}`,
+    )
     .then((result) => {
       let relatedIdArray = [];
       for (let i = 0; i < result.rows.length; i++) {
@@ -127,7 +123,9 @@ app.get('/products/:product_id/related', (req, res) => {
       }
       res.status(200).send(relatedIdArray);
     })
-    .catch((err) => res.status(500).send(`Error getting details of product: ${err.message}`));
+    .catch((err) =>
+      res.status(500).send(`Error getting details of product: ${err.message}`),
+    );
 });
 
 // app.get('/cart', (req, res) => {
@@ -146,5 +144,5 @@ app.get('/products/:product_id/related', (req, res) => {
 // });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
+  console.log(`Server is listening on port ${PORT}`);
 });
